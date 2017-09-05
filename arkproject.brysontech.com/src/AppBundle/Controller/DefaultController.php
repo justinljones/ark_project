3cfd0eb576aa3e9b006bbed47b5afd96bd30e0ff
JJones <jjones@brysontech.com>
2017-09-05 14:41:57 -0400

    Added a Registration feature to the main page. Also added a /register route to the controller leading to a register page that displays the results of the register form.

--- arkproject.brysontech.com/src/AppBundle/Controller/DefaultController.php
+++ arkproject.brysontech.com/src/AppBundle/Controller/DefaultController.php
@@ -24,9 +24,22 @@ class DefaultController extends Controller
      */
     public function loginAction (Request $request)
     {
-        return $this->render('Login/login.html.twig',[
-            $username = isset($_POST['username'])   ?$_POST['username'] : false,
-            $pwd      = $_POST['pwd']
+        return $this->render('Login/login.html.twig',[                
+            'username' => isset($_POST['username'])   ?$_POST['username'] : false,
+            'pwd'      => isset($_POST['pwd'])        ?$_POST['pwd'] :false
+        ]);
+    }
+    
+    /**
+     * @Route ("/register", name="register")
+     */
+    public function registerAction (Request $request)
+    {
+        return $this->render('Register/register.html.twig',[
+            'username' => isset($_POST['username'])   ?$_POST['username'] : false,
+            'pwd'      => isset($_POST['pwd'])        ?$_POST['pwd']      : false,
+            'Cpwd'     => isset($_POST['Cpwd'])       ?$_POST['Cpwd']     : false,
+            'email'    => isset($_POST['email'])      ?$_POST['email']    : false
         ]);
     }
 }
